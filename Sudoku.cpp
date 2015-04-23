cca9ddae67621076b4f12bcdf0e7e57ed8c1e6de
litingyang <F74032023@mail.ncku.edu.tw>
2015-04-23 20:15:25 +0800

    change

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -7,8 +7,22 @@ using namespace std;
 void Sudoku::GiveQuestion(){
 	srand((unsigned)time(NULL));
 	int a,b;
-	b=rand()%4;
-	int arr[4][144]={
+	b=rand()%5;
+	int arr[5][144]={
+		0,2,0,0,0,0,9,0,1,-1,-1,-1,
+		0,0,0,0,0,1,0,0,4,-1,-1,-1,
+		0,5,0,6,0,0,0,7,0,-1,-1,-1,
+		-1,-1,-1,0,3,0,0,8,0,0,0,0,
+		-1,-1,-1,7,0,5,0,0,0,4,0,0,
+		-1,-1,-1,0,9,0,0,0,0,0,7,1,
+		1,0,7,0,4,0,-1,-1,-1,0,0,5,
+		0,0,4,0,0,9,-1,-1,-1,0,3,0,
+		0,0,0,0,0,0,-1,-1,-1,2,8,0,
+		0,0,0,-1,-1,-1,0,4,0,0,6,0,
+		7,0,0,-1,-1,-1,0,0,9,0,0,0,
+		0,6,0,-1,-1,-1,3,0,0,0,0,8,
+
+
 		0,2,6,0,0,0,0,5,0,-1,-1,-1,
 		0,0,0,9,0,0,6,0,4,-1,-1,-1,
 		9,0,0,0,2,4,0,0,3,-1,-1,-1,
@@ -48,8 +62,8 @@ void Sudoku::GiveQuestion(){
 		3,1,2,-1,-1,-1,7,4,8,0,0,0,
 		7,4,8,-1,-1,-1,5,6,9,0,0,0,
 		5,6,9,-1,-1,-1,3,1,2,0,0,0,
-		
-		
+
+
 		0,2,6,8,7,3,0,5,1,-1,-1,-1,
 		8,7,3,9,0,0,6,0,4,-1,-1,-1,
 		9,0,0,0,2,4,0,7,3,-1,-1,-1,
@@ -112,73 +126,53 @@ void Sudoku::Solve(){
 				cout<<endl;
 		}
 	}
-return;}
+	return;}
 
-int Sudoku::check(int (&array)[144],bool (&where)[144][9]){
+	int Sudoku::check(int (&array)[144],bool (&where)[144][9]){
 
-	int z,a,x,w,u,v,tmp[144];
-	for(a=0;a<144;a++)
-	{if(array[a]==0&&(where[a][0]+where[a][1]+where[a][2]+where[a][3]+where[a][4]+where[a][5]+where[a][6]+where[a][7]+where[a][8])==8){
-																		  for(z=0;z<9;z++){
-																			  if(where[a][z]==false){
-																				  array[a]=z+1;
-																				  for(x=0;x<12;x++){
-																					  where[(a/12)*12+x][z]=1;
-																				  }
-																				  for(x=0;x<12;x++){
-																					  where[(a%12)+12*x][z]=1;
-																				  }
-																				  for(x=0;x<3;x++){
-																					  for(w=0;w<3;w++){
-																						  where[((a/12)/3)*36+((a%12)/3)*3+x*12+w][z]=1;
+		int z,a,x,w,u,v,tmp[144];
+		for(a=0;a<144;a++)
+		{if(array[a]==0&&(where[a][0]+where[a][1]+where[a][2]+where[a][3]+where[a][4]+where[a][5]+where[a][6]+where[a][7]+where[a][8])==8){
+																			  for(z=0;z<9;z++){
+																				  if(where[a][z]==false){
+																					  array[a]=z+1;
+																					  for(x=0;x<12;x++){
+																						  where[(a/12)*12+x][z]=1;
+																					  }
+																					  for(x=0;x<12;x++){
+																						  where[(a%12)+12*x][z]=1;
+																					  }
+																					  for(x=0;x<3;x++){
+																						  for(w=0;w<3;w++){
+																							  where[((a/12)/3)*36+((a%12)/3)*3+x*12+w][z]=1;
+																						  }
 																					  }
 																				  }
 																			  }
 																		  }
-																	  }
-	}
-	for(a=0;a<144;a++){
-		if(array[a]==0&&(where[a][0]+where[a][1]+where[a][2]+where[a][3]+where[a][4]+where[a][5]+where[a][6]+where[a][7]+where[a][8])==8){
-			check(array,where);
 		}
-	}
-	if(checkans1(array)==1&&checkans2(array)==1&&checkans3(array)==1)return 1;
-	else{
-	for(a=0;a<144;a++){
-		if(array[a]==0){
-			for(z=0;z<9;z++){
-				if(where[a][z]==false){
-					return 2;
-				}
-
+		for(a=0;a<144;a++){
+			if(array[a]==0&&(where[a][0]+where[a][1]+where[a][2]+where[a][3]+where[a][4]+where[a][5]+where[a][6]+where[a][7]+where[a][8])==8){
+				check(array,where);
 			}
-			return 0;
 		}
-	}
-	}
-	return 0;}
+		if(checkans1(array)==1&&checkans2(array)==1&&checkans3(array)==1)return 1;
+		else{
+			for(a=0;a<144;a++){
+				if(array[a]==0){
+					for(z=0;z<9;z++){
+						if(where[a][z]==false){
+							return 2;
+						}
 
-	int Sudoku::checkans1(int (&array)[144]){
-		int nine[9];
-		int x,n,y,z;				
-		for(x=0;x<12;x++)
-		{
-			for(n=0;n<9;n++){
-				nine[n]=0;
-			}
-			for(y=0;y<12;y++){
-				for(z=0;z<9;z++){
-					if(array[12*x+y]==z+1)
-						nine[z]= nine[z]+1;
-				}
-			}
-			for(n=0;n<9;n++){
-				if(nine[n]!=1)
+					}
 					return 0;
+				}
 			}
 		}
-		return 1	;}
-		int Sudoku::checkans2(int (&array)[144]){
+		return 0;}
+
+		int Sudoku::checkans1(int (&array)[144]){
 			int nine[9];
 			int x,n,y,z;				
 			for(x=0;x<12;x++)
@@ -188,7 +182,7 @@ int Sudoku::check(int (&array)[144],bool (&where)[144][9]){
 				}
 				for(y=0;y<12;y++){
 					for(z=0;z<9;z++){
-						if(array[12*y+x]!=-1&&array[12*y+x]==z+1)
+						if(array[12*x+y]==z+1)
 							nine[z]= nine[z]+1;
 					}
 				}
@@ -197,36 +191,56 @@ int Sudoku::check(int (&array)[144],bool (&where)[144][9]){
 						return 0;
 				}
 			}
-			return 1;	}
-			int Sudoku::checkans3(int (&array)[144]){
+			return 1	;}
+			int Sudoku::checkans2(int (&array)[144]){
 				int nine[9];
-				int x,n,y,z,u,v;				
-				for(x=0;x<4;x++)
+				int x,n,y,z;				
+				for(x=0;x<12;x++)
 				{
-					for(v=0;v<4;v++){
-						for(n=0;n<9;n++){
-							nine[n]=0;
+					for(n=0;n<9;n++){
+						nine[n]=0;
+					}
+					for(y=0;y<12;y++){
+						for(z=0;z<9;z++){
+							if(array[12*y+x]!=-1&&array[12*y+x]==z+1)
+								nine[z]= nine[z]+1;
 						}
-						for(u=0;u<3;u++){
-							for(y=0;y<3;y++){
-								for(z=0;z<9;z++){
+					}
+					for(n=0;n<9;n++){
+						if(nine[n]!=1)
+							return 0;
+					}
+				}
+				return 1;	}
+				int Sudoku::checkans3(int (&array)[144]){
+					int nine[9];
+					int x,n,y,z,u,v;				
+					for(x=0;x<4;x++)
+					{
+						for(v=0;v<4;v++){
+							for(n=0;n<9;n++){
+								nine[n]=0;
+							}
+							for(u=0;u<3;u++){
+								for(y=0;y<3;y++){
+									for(z=0;z<9;z++){
 
-									if(array[x*36+v*3+12*u+y]!=-1&&array[x*36+v*3+12*u+y]==z+1)
-										nine[z]= nine[z]+1;
-									if(array[x*36+v*3]==-1){
-										nine[z]=1;
-										if(array[x*36+v*3+12*0+1]!=-1||array[x*36+v*3+12*0+2]!=-1||array[x*36+v*3+12*1+0]!=-1||array[x*36+v*3+12*1+1]!=-1||array[x*36+v*3+12*1+2]!=-1||array[x*36+v*3+12*2+0]!=-1||array[x*36+v*3+12*2+1]!=-1||array[x*36+v*3+12*2+2]!=-1)
-											return 0;
+										if(array[x*36+v*3+12*u+y]!=-1&&array[x*36+v*3+12*u+y]==z+1)
+											nine[z]= nine[z]+1;
+										if(array[x*36+v*3]==-1){
+											nine[z]=1;
+											if(array[x*36+v*3+12*0+1]!=-1||array[x*36+v*3+12*0+2]!=-1||array[x*36+v*3+12*1+0]!=-1||array[x*36+v*3+12*1+1]!=-1||array[x*36+v*3+12*1+2]!=-1||array[x*36+v*3+12*2+0]!=-1||array[x*36+v*3+12*2+1]!=-1||array[x*36+v*3+12*2+2]!=-1)
+												return 0;
+										}
 									}
 								}
 							}
-						}
 
-						for(n=0;n<9;n++){
-							if(nine[n]!=1)
-								return 0;
-						}
-					}	
+							for(n=0;n<9;n++){
+								if(nine[n]!=1)
+									return 0;
+							}
+						}	
 
-				}
-				return 1;	}
+					}
+					return 1;	}
